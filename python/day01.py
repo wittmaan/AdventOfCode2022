decf82c97bc2c737e91bc041cc6d0a4dae6a82c8
wittmaan <wittmaan@gmx.de>
2022-12-01 21:02:27 +0100

    some improvements

--- python/day01.py
+++ python/day01.py
@@ -1,5 +1,5 @@
 import fileinput
-from typing import List, Tuple
+from typing import List
 
 # --- Day 1: Calorie Counting ---
 # --- Part one ---
@@ -22,42 +22,25 @@ sample_input = """1000
 )
 
 
-def get_most_calories(dat: List[List[str]]) -> Tuple[int, int]:
-    most_calories = None
-    idx_most_calories = None
-    for idx, val in enumerate(dat):
-        result = val.split("\n")
-        result = sum([int(_) for _ in result])
-
-        if most_calories is None or result > most_calories:
-            most_calories = result
-            idx_most_calories = idx
-    return most_calories, idx_most_calories
+def get_top_most_calories(dat: List[List[str]], number=1) -> int:
+    calories_per_elf = []
+    for val in dat:
+        calories_per_elf.append(sum([int(_) for _ in val.split("\n")]))
+    return sum(sorted(calories_per_elf, reverse=True)[:number])
 
 
-assert get_most_calories(sample_input) == (24000, 3)
+assert get_top_most_calories(sample_input) == 24000
 
 puzzle_input = ("".join([_ for _ in fileinput.input()])).split("\n\n")
-solution_part1 = get_most_calories(puzzle_input)
+solution_part1 = get_top_most_calories(puzzle_input)
 
-assert solution_part1 == (72478, 122)
+assert solution_part1 == 72478
 print(f"solution part1: {solution_part1}")
 
 
 # --- Part two ---
 
-
-def get_top_most_calories(dat: List[List[str]], number=3) -> int:
-    calories_per_elf = []
-    for idx, val in enumerate(dat):
-        result = val.split("\n")
-        result = sum([int(_) for _ in result])
-        calories_per_elf.append(result)
-
-    return sum(sorted(calories_per_elf, reverse=True)[:number])
-
-
-assert get_top_most_calories(sample_input) == 45000
-solution_part2 = get_top_most_calories(puzzle_input)
+assert get_top_most_calories(sample_input, number=3) == 45000
+solution_part2 = get_top_most_calories(puzzle_input, number=3)
 assert solution_part2 == 210367
 print(f"solution part2: {solution_part2}")
