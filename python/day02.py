83b243b454939da63ff241780dce7c6103c39836
wittmaan <wittmaan@gmx.de>
2022-12-02 20:44:16 +0100

    solution day 2

--- python/day02.py
+++ python/day02.py
@@ -10,6 +10,10 @@ C Z""".split(
     "\n"
 )
 
+LOSE = 0
+DRAW = 3
+WIN = 6
+
 ROCK = "ROCK"
 PAPER = "PAPER"
 SCISSORS = "Scissors"
@@ -19,21 +23,29 @@ OTHER_MAPPING = {"A": ROCK, "B": PAPER, "C": SCISSORS}
 OWN_MAPPING = {"X": ROCK, "Y": PAPER, "Z": SCISSORS}
 
 
-def play(dat: List[str]) -> int:
+def play(dat: List[str], mode="part1") -> int:
     other_val = STRATEGY_GUIDE[OTHER_MAPPING[dat[0]]]
     own_val = STRATEGY_GUIDE[OWN_MAPPING[dat[1]]]
 
-    difference = (other_val - own_val) % 3
-    if difference == 2:
-        return own_val + 6
-    elif difference == 0:
-        return own_val + 3
-    elif difference == 1:
-        return own_val + 0
+    if mode == "part1":
+        difference = (other_val - own_val) % 3
+        if difference == 0:
+            return own_val + DRAW
+        elif difference == 1:
+            return own_val + LOSE
+        elif difference == 2:
+            return own_val + WIN
+    else:
+        if own_val == 2:
+            return other_val + DRAW
+        elif own_val == 1:
+            return (other_val - 2) % 3 + 1 + LOSE
+        elif own_val == 3:
+            return other_val % 3 + 1 + WIN
 
 
-def calc_total_score(dat: List[str]) -> int:
-    return sum([play(_.split()) for _ in dat])
+def calc_total_score(dat: List[str], mode="part1") -> int:
+    return sum([play(_.split(), mode) for _ in dat])
 
 
 assert calc_total_score(sample_input) == 15
@@ -47,6 +59,8 @@ print(f"solution part1: {solution_part1}")
 
 # --- Part two ---
 
+assert calc_total_score(sample_input, mode="part2") == 12
+solution_part2 = calc_total_score(puzzle_input, mode="part2")
 
-# assert solution_part2 == 210367
-# print(f"solution part2: {solution_part2}")
+assert solution_part2 == 13889
+print(f"solution part2: {solution_part2}")
