9abe99bbc29bc6b1dabcc79093339fd3beb3d6b2
wittmaan <wittmaan@gmx.de>
2022-12-04 18:29:38 +0100

    solution day 4

--- python/day04.py
+++ python/day04.py
@@ -1,8 +1,4 @@
 import fileinput
-import string
-from collections import Counter
-from functools import reduce
-from operator import and_
 from typing import List
 
 # --- Day 4: Camp Cleanup ---
@@ -18,19 +14,28 @@ sample_input = """2-4,6-8
 )
 
 
-def check_contain(first_pair: str, second_pair: str) -> int:
+def check_contain(first_pair: str, second_pair: str, mode="part1") -> int:
     first_pair = [int(_) for _ in first_pair.split("-")]
     second_pair = [int(_) for _ in second_pair.split("-")]
 
-    if (second_pair[0] >= first_pair[0] and second_pair[1] <= first_pair[1]) or (
-        first_pair[0] >= second_pair[0] and first_pair[1] <= second_pair[1]
-    ):
-        return 1
+    if mode == "part1":
+        if (second_pair[0] >= first_pair[0] and second_pair[1] <= first_pair[1]) or (
+            first_pair[0] >= second_pair[0] and first_pair[1] <= second_pair[1]
+        ):
+            return 1
+    else:
+        if (
+            first_pair[0] <= second_pair[0] <= first_pair[1]
+            or second_pair[0] <= first_pair[0] <= second_pair[1]
+            or first_pair[0] <= second_pair[1] <= first_pair[1]
+            or second_pair[0] <= first_pair[1] <= second_pair[1]
+        ):
+            return 1
 
     return 0
 
 
-def calc_sum_fully_contain(dat: List[str]):
+def calc_sum_fully_contain(dat: List[str]) -> int:
     return sum([check_contain(*_.split(",")) for _ in dat])
 
 
@@ -45,14 +50,13 @@ print(f"solution part1: {solution_part1}")
 
 # --- Part two ---
 
-#
-# def split_lines(dat: List[str], chunk_size=3):
-#     for i in range(0, len(dat), chunk_size):
-#         yield dat[i : i + chunk_size]
-#
-#
-# assert calc_priority_sum(split_lines(sample_input), mode="part2") == 70
-#
-# solution_part2 = calc_priority_sum(split_lines(puzzle_input), mode="part2")
-# assert solution_part2 == 2668
-# print(f"solution part2: {solution_part2}")
+
+def calc_ranges_overlap(dat: List[str]) -> int:
+    return sum([check_contain(*_.split(","), mode="part2") for _ in dat])
+
+
+assert calc_ranges_overlap(sample_input) == 4
+
+solution_part2 = calc_ranges_overlap(puzzle_input)
+assert solution_part2 == 798
+print(f"solution part2: {solution_part2}")
