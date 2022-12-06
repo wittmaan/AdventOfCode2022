452b2b77757f8fe244eebac88ab398b0a972fde4
wittmaan <wittmaan@gmx.de>
2022-12-06 19:11:26 +0100

    solution day 6

--- python/day06.py
+++ python/day06.py
@@ -1,3 +1,8 @@
+import fileinput
+
+# --- Day 6: Tuning Trouble ---
+# --- Part one ---
+
 sample_input1 = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
 sample_input2 = "bvwbjplbgvbhsrlpgdmjqwftvncz"
 sample_input3 = "nppdvjthqldpwncqszvftbrmjlhg"
@@ -17,3 +22,17 @@ assert detect_marker(sample_input2) == 5
 assert detect_marker(sample_input3) == 6
 assert detect_marker(sample_input4) == 10
 assert detect_marker(sample_input5) == 11
+
+puzzle_input = [_ for _ in fileinput.input()][0]
+
+solution_part1 = detect_marker(puzzle_input)
+
+assert solution_part1 == 1343
+print(f"solution part1: {solution_part1}")
+
+# --- Part two ---
+
+solution_part2 = detect_marker(puzzle_input, limit=14)
+
+assert solution_part1 == 2193
+print(f"solution part1: {solution_part2}")
